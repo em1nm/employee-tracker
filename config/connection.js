6d4319f252b56bf958f114252cb8c1a04b8ecc26
em1nm <em.martirosyan@gmail.com>
2023-05-10 14:17:13 -0700

    created connection

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv').config();
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+},
+    console.log(`SQL Connection established.`));
+    
+module.exports = db;
+
